194b9867e79cbdb34ce2f58a4633c3b7ae0f54b4
unknown <kodytran854@gmail.com>
2022-11-17 01:18:23 -0500

    first commit

---
+++ src/components/DashBoard.js
@@ -0,0 +1,13 @@
+
+const DashBoard = ()=>{
+ return(
+ <div className="topBar">
+    <div className="topBarWrapper">
+    <div className="topLeft"></div>
+    <div className="visitors"></div>
+    </div>
+
+</div>
+    )
+}
+export default DashBoard;
